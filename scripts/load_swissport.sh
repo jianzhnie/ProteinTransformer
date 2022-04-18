198a4a9d224d2571eeb2662f04957bf4e94a0178
Robin <jianzhnie@126.com>
2022-04-18 18:55:22 +0800

    update scripts

---
+++ scripts/load_swissport.sh
@@ -0,0 +1,2 @@
+#!/bin/bash/
+python deepfold/data/load_swissport.py --go-file data/go.obo --swissprot-file  data/uniprot_sprot.dat.gz --out-file data/data/swissprot.pkl
---
+++ scripts/train_model.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python function_predcit.py \
+        --data-path /Users/robin/xbiome/DeepFold/data/data \
+        --model-path /Users/robin/xbiome/DeepFold/work_dir/model/deepgoplus \
+        --summary-path /Users/robin/xbiome/DeepFold/work_dir/logs/deepgoplus
+       
+ 
