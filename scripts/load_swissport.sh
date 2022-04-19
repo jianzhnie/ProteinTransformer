314e741f619fb507abb021e08e1f1268549acb7e
Robin <jianzhnie@126.com>
2022-04-19 17:30:51 +0800

    Update load_swissport.sh

--- scripts/load_swissport.sh
+++ scripts/load_swissport.sh
@@ -1,2 +1,5 @@
 #!/bin/bash/
-python deepfold/data/load_swissport.py --go-file data/go.obo --swissprot-file  data/uniprot_sprot.dat.gz --out-file data/data/swissprot.pkl
+python deepfold/data/load_swissport.py \
+        --go-file data/go.obo \
+        --swissprot-file  data/uniprot_sprot.dat.gz \
+        --out-file data/data/swissprot.pkl
