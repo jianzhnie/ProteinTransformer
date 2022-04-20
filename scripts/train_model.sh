860d3b3fdb38e80accf262b1b0531960314c39ee
Robin <jianzhnie@126.com>
2022-04-20 23:34:04 +0800

    Update train_model.sh

--- scripts/train_model.sh
+++ scripts/train_model.sh
@@ -5,3 +5,10 @@ python function_predcit.py \
         --model-path /Users/robin/xbiome/DeepFold/work_dir/model/deepgoplus \
         --summary-path /Users/robin/xbiome/DeepFold/work_dir/logs/deepgoplus
        
+
+CUDA_LAUNCH_BLOCKING=1
+python function_predcit.py \
+        --data-path /home/niejianzheng/xbiome/DeepFold/data/data \
+        --model-path /home/niejianzheng/xbiome/DeepFold/work_dir/model/deepgoplus \
+        --summary-path home/niejianzheng/xbiome/DeepFold/work_dir/logs/deepgoplus
+       
