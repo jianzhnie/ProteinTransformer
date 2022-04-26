296d4f2b23f22dcdc30a9f5767bc418dbeb72cd9
Robin <jianzhnie@126.com>
2022-04-26 17:04:43 +0800

    Update protein_dataset.py

--- deepfold/data/protein_dataset.py
+++ deepfold/data/protein_dataset.py
@@ -5,6 +5,37 @@ from protein_tokenizer import ProteinTokenizer
 from torch.utils.data import Dataset
 
 
+class ProteinSequenceDataset(Dataset):
+    def __init__(self, sequence, targets, tokenizer, max_len):
+        self.sequence = sequence
+        self.targets = targets
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.sequence)
+
+    def __getitem__(self, item):
+        sequence = str(self.sequence[item])
+        target = self.targets[item]
+        encoding = self.tokenizer.encode_plus(
+            sequence,
+            truncation=True,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            return_token_type_ids=False,
+            padding='max_length',
+            return_attention_mask=True,
+            return_tensors='pt',
+        )
+        return {
+            'protein_sequence': sequence,
+            'input_ids': encoding['input_ids'].flatten(),
+            'attention_mask': encoding['attention_mask'].flatten(),
+            'targets': torch.tensor(target, dtype=torch.long)
+        }
+
+
 class ProteinSequences(Dataset):
     def __init__(self, data_file, terms_file):
         super().__init__()
