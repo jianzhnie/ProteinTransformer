0180c87f012b1c4be2810436d3ee64a3f83bdeab
Robin <jianzhnie@126.com>
2022-04-27 18:30:51 +0800

    update scripts

---
+++ scripts/ditribute_training.sh
@@ -0,0 +1,13 @@
+python function_predcit.py \
+        --data-path /Users/robin/xbiome/DeepFold/data/data \
+        --model-path /Users/robin/xbiome/DeepFold/work_dir/model/deepgoplus \
+        --summary-path /Users/robin/xbiome/DeepFold/work_dir/logs/deepgoplus
+
+
+python main.py \
+--data_path /Users/robin/xbiome/datasets/protein \
+--output-dir /Users/robin/xbiome/DeepFold/work_dir \
+--epochs 2 \
+--lr 0.001 \
+--epochs 10 \
+--batch-size 256
