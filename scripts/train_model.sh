ac7f96d4a26276933670e3df92c14b7a1042d40b
Robin <jianzhnie@126.com>
2022-05-04 14:41:55 +0800

    Update train_model.sh

--- scripts/train_model.sh
+++ scripts/train_model.sh
@@ -4,10 +4,9 @@ python function_predcit.py \
         --data-path /Users/robin/xbiome/DeepFold/data/data \
         --model-path /Users/robin/xbiome/DeepFold/work_dir/model/deepgoplus \
         --summary-path /Users/robin/xbiome/DeepFold/work_dir/logs/deepgoplus
-       
+
 
 python function_predcit.py \
         --data-path /home/niejianzheng/xbiome/DeepFold/data/data \
         --model-path /home/niejianzheng/xbiome/DeepFold/work_dir/model/deepgoplus \
         --summary-path home/niejianzheng/xbiome/DeepFold/work_dir/logs/deepgoplus
-       
