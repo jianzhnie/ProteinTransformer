2e73da9ca2e115d4afb9cca07b9d73762d36ed58
jianzhnie <jianzhnie@126.com>
2022-05-06 09:05:27 +0200

    update protlm model

--- tools/protlm.py
+++ tools/protlm.py
@@ -1,18 +1,16 @@
 import sys
-
-from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support, f1_score
 from transformers import BertConfig, Trainer, TrainingArguments
-
+sys.path.append('../')
 from deepfold.data.protein_dataset import ProtBertDataset
 from deepfold.models.transformers.multilabel_transformer import \
     BertForMultiLabelSequenceClassification
 
-sys.path.append('../')
 
 
-def compute_metrics(pred):
+def compute_metrics_(pred):
     labels = pred.label_ids
-    preds = pred.predictions.argmax(-1)
+    preds = pred.predictions
     precision, recall, f1, _ = precision_recall_fscore_support(
         labels, preds, average='binary')
     acc = accuracy_score(labels, preds)
@@ -23,6 +21,23 @@ def compute_metrics(pred):
         'recall': recall
     }
 
+def compute_metrics(pred):
+    print(pred)
+    labels = pred.label_ids
+    print(labels)
+    preds = pred.predictions.argmax(-1)
+    print(preds)
+    print(labels.shape, preds.shape)
+    accuracy = accuracy_score(labels, preds)
+    f1_score_micro = f1_score(labels, preds, average='micro')
+    f1_score_macro = f1_score(labels, preds, average='macro')
+    return {
+        'accuracy': accuracy,
+        'f1_score_micro': f1_score_micro,
+        'f1_score_macro': f1_score_macro
+    }
+
+
 
 if __name__ == '__main__':
     model_name = 'Rostlab/prot_bert_bfd'
@@ -31,15 +46,15 @@ if __name__ == '__main__':
         data_path=data_root,
         split='train',
         tokenizer_name=model_name,
-        max_length=1024)  # max_length is only capped to speed-up example.
-    val_dataset = ProtBertDataset(data_path=data_root,
+        max_length=128)  # max_length is only capped to speed-up example.
+    train_dataset = ProtBertDataset(data_path=data_root,
                                   split='valid',
                                   tokenizer_name=model_name,
-                                  max_length=1024)
-    test_dataset = ProtBertDataset(data_path=data_root,
+                                  max_length=128)
+    val_dataset = ProtBertDataset(data_path=data_root,
                                    split='test',
                                    tokenizer_name=model_name,
-                                   max_length=1024)
+                                   max_length=128)
     num_classes = train_dataset.num_classes
     model_config = BertConfig.from_pretrained(model_name,
                                               num_labels=num_classes)
@@ -50,8 +65,8 @@ if __name__ == '__main__':
     training_args = TrainingArguments(
         output_dir='./work_dir',  # output directory
         num_train_epochs=1,  # total number of training epochs
-        per_device_train_batch_size=32,  # batch size per device during training
-        per_device_eval_batch_size=32,  # batch size for evaluation
+        per_device_train_batch_size=64,  # batch size per device during training
+        per_device_eval_batch_size=64,  # batch size for evaluation
         warmup_steps=1000,  # number of warmup steps for learning rate scheduler
         weight_decay=0.01,  # strength of weight decay
         logging_dir='./logs',  # directory for storing logs
@@ -61,8 +76,8 @@ if __name__ == '__main__':
         evaluation_strategy='epoch',  # evalute after eachh epoch
         gradient_accumulation_steps=1,
         # total number of steps before back propagation
-        # fp16=True,  # Use mixed precision
-        # fp16_opt_level="02",  # mixed precision mode
+        fp16=True,  # Use mixed precision
+        fp16_opt_level="02",  # mixed precision mode
         run_name='ProBert-BFD-MS',  # experiment name
         seed=3  # Seed for experiment reproducibility 3x3
     )
