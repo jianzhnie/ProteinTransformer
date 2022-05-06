d3b28f9e3946bca8abe7321e683bc0603143b4fd
Robin <jianzhnie@126.com>
2022-05-06 15:13:35 +0800

    Update multilabel_transformer.py

--- deepfold/models/transformers/multilabel_transformer.py
+++ deepfold/models/transformers/multilabel_transformer.py
@@ -1,4 +1,7 @@
+from typing import Optional, Tuple
+
 import torch
+import torch.nn.functional as F
 from torch import nn
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 from transformers import (BertModel, BertPreTrainedModel, DistilBertModel,
@@ -32,15 +35,50 @@ from transformers.models.xlm_roberta.configuration_xlm_roberta import \
     XLMRobertaConfig
 from transformers.models.xlm_roberta.modeling_xlm_roberta import \
     XLM_ROBERTA_PRETRAINED_MODEL_ARCHIVE_LIST
+from transformers.utils.generic import ModelOutput
+
+
+class SequenceClassifierOutput(ModelOutput):
+    """Base class for outputs of sentence classification models.
+
+    Args:
+        loss (`torch.FloatTensor` of shape `(1,)`, *optional*, returned when `labels` is provided):
+            Classification (or regression if config.num_labels==1) loss.
+        logits (`torch.FloatTensor` of shape `(batch_size, config.num_labels)`):
+            Classification (or regression if config.num_labels==1) scores (before SoftMax).
+        preds (`torch.FloatTensor` of shape `(batch_size, config.num_labels)`):
+            Classification (or regression if config.num_labels==1) labels.
+        hidden_states (`tuple(torch.FloatTensor)`, *optional*, returned when `output_hidden_states=True` is passed or when `config.output_hidden_states=True`):
+            Tuple of `torch.FloatTensor` (one for the output of the embeddings, if the model has an embedding layer, +
+            one for the output of each layer) of shape `(batch_size, sequence_length, hidden_size)`.
+
+            Hidden-states of the model at the output of each layer plus the optional initial embedding outputs.
+        attentions (`tuple(torch.FloatTensor)`, *optional*, returned when `output_attentions=True` is passed or when `config.output_attentions=True`):
+            Tuple of `torch.FloatTensor` (one for each layer) of shape `(batch_size, num_heads, sequence_length,
+            sequence_length)`.
+
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+    """
+
+    loss: Optional[torch.FloatTensor] = None
+    logits: torch.FloatTensor = None
+    preds: torch.FloatTensor = None
+    hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+    attentions: Optional[Tuple[torch.FloatTensor]] = None
 
 
 class BertForMultiLabelSequenceClassification(BertPreTrainedModel):
     """Bert model adapted for multi-label sequence classification."""
     def __init__(self, config, pos_weight=None):
         super(BertForMultiLabelSequenceClassification, self).__init__(config)
+        self.config = config
         self.num_labels = config.num_labels
         self.bert = BertModel(config)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        classifier_dropout = (config.classifier_dropout
+                              if config.classifier_dropout is not None else
+                              config.hidden_dropout_prob)
+        self.dropout = nn.Dropout(classifier_dropout)
         self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
         self.pos_weight = pos_weight
 
@@ -54,22 +92,27 @@ class BertForMultiLabelSequenceClassification(BertPreTrainedModel):
         position_ids=None,
         head_mask=None,
         labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
     ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
         outputs = self.bert(
             input_ids,
             attention_mask=attention_mask,
             token_type_ids=token_type_ids,
             position_ids=position_ids,
             head_mask=head_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
         )
 
         pooled_output = outputs[1]
 
         pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
-
-        outputs = (logits, ) + outputs[2:]
-        # add hidden states and attention if they are here
 
         if labels is not None:
             loss_fct = BCEWithLogitsLoss(pos_weight=self.pos_weight)
@@ -78,7 +121,19 @@ class BertForMultiLabelSequenceClassification(BertPreTrainedModel):
                             labels.view(-1, self.num_labels))
             outputs = (loss, ) + outputs
 
-        return outputs  # (loss), logits, (hidden_states), (attentions)
+            preds = F.sigmoid(logits)
+
+        if not return_dict:
+            output = (logits, ) + (preds, ) + outputs[2:]
+            return ((loss, ) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            preds=preds,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
 
 
 class RobertaForMultiLabelSequenceClassification(BertPreTrainedModel):
