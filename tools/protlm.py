56f9c58bfe01f6e739fcc51d0a07be6ba78354da
Robin <jianzhnie@126.com>
2022-05-07 16:46:23 +0800

    Update protlm.py

--- tools/protlm.py
+++ tools/protlm.py
@@ -1,12 +1,15 @@
 import sys
 
 from sklearn.metrics import average_precision_score, roc_auc_score
-from transformers import BertConfig, Trainer, TrainingArguments
-sys.path.append('../')
+from torch.optim import AdamW
+from transformers import (BertConfig, EarlyStoppingCallback, Trainer,
+                          TrainingArguments)
 
 from deepfold.data.protein_dataset import ProtBertDataset
 from deepfold.models.transformers.multilabel_transformer import \
     BertForMultiLabelSequenceClassification
+
+sys.path.append('../')
 
 
 def compute_metrics(pred):
@@ -39,9 +42,25 @@ if __name__ == '__main__':
     model = BertForMultiLabelSequenceClassification.from_pretrained(
         model_name, config=model_config)
 
+    # setting custom optimization parameters. You may implement a scheduler here as well.
+    param_optimizer = list(model.named_parameters())
+    no_decay = ['bias', 'gamma', 'beta']
+    optimizer_grouped_parameters = [{
+        'params':
+        [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+        'weight_decay_rate':
+        0.01
+    }, {
+        'params':
+        [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+        'weight_decay_rate':
+        0.0
+    }]
+    optimizer = AdamW(optimizer_grouped_parameters, lr=2e-5, correct_bias=True)
+
     training_args = TrainingArguments(
         output_dir='./work_dir',  # output directory
-        num_train_epochs=2,  # total number of training epochs
+        num_train_epochs=30,  # total number of training epochs
         per_device_train_batch_size=16,  # batch size per device during training
         per_device_eval_batch_size=16,  # batch size for evaluation
         warmup_steps=1000,  # number of warmup steps for learning rate scheduler
@@ -55,7 +74,9 @@ if __name__ == '__main__':
         # total number of steps before back propagation
         fp16=True,  # Use mixed precision
         fp16_opt_level='02',  # mixed precision mode
+        report_to='wandb',  # enable logging to W&B
         run_name='ProBert-BFD-MS',  # experiment name
+        load_best_model_at_end=True,
         seed=3  # Seed for experiment reproducibility 3x3
     )
 
@@ -64,9 +85,16 @@ if __name__ == '__main__':
         args=training_args,  # training arguments, defined above
         train_dataset=train_dataset,  # training dataset
         eval_dataset=val_dataset,  # evaluation dataset
-        compute_metrics=compute_metrics,  # evaluation metrics
+        # compute_metrics=compute_metrics,  # evaluation metrics
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
     )
 
     trainer.train()
     trainer.save_model('models/')
+    # Load trained model
+    model_path = 'work_dir/checkpoint-50000'
+    model = BertForMultiLabelSequenceClassification.from_pretrained(
+        model_path, num_labels=num_classes)
+    # Define test trainer
+    test_trainer = Trainer(model)
     predictions, label_ids, metrics = trainer.predict(val_dataset)
