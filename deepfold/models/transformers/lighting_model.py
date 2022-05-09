bf50013fe31cb3e54d9f55c6feced250345fdf07
Robin <jianzhnie@126.com>
2022-05-09 17:42:01 +0800

    Update lighting_model.py

--- deepfold/models/transformers/lighting_model.py
+++ deepfold/models/transformers/lighting_model.py
@@ -4,9 +4,74 @@ from typing import Dict, List, Tuple
 
 import pytorch_lightning as pl
 import torch
-import torch.nn as nn
+from torch import nn as nn
+from torch.nn import BCEWithLogitsLoss
 from transformers import (AutoConfig, AutoModelForSequenceClassification,
+                          BertModel, BertPreTrainedModel,
                           get_linear_schedule_with_warmup)
+from transformers.modeling_outputs import SequenceClassifierOutput
+
+
+class BertForMultiLabelSequenceClassification(BertPreTrainedModel):
+    """Bert model adapted for multi-label sequence classification."""
+    def __init__(self, config, pos_weight=None):
+        super(BertForMultiLabelSequenceClassification, self).__init__(config)
+        self.config = config
+        self.num_labels = config.num_labels
+        self.bert = BertModel(config)
+        classifier_dropout = (config.classifier_dropout
+                              if config.classifier_dropout is not None else
+                              config.hidden_dropout_prob)
+        self.dropout = nn.Dropout(classifier_dropout)
+        self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
+        self.pos_weight = pos_weight
+
+    def forward(
+        self,
+        input_ids,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        if labels is not None:
+            loss_fct = BCEWithLogitsLoss(pos_weight=self.pos_weight)
+            labels = labels.float()
+            loss = loss_fct(logits.view(-1, self.num_labels),
+                            labels.view(-1, self.num_labels))
+
+        if not return_dict:
+            output = (logits, ) + outputs[2:]
+            return ((loss, ) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
 
 
 class LightningTransformer(pl.LightningModule):
@@ -29,10 +94,6 @@ class LightningTransformer(pl.LightningModule):
 
         self.save_hyperparameters()
         if model is not None:
-            assert isinstance(
-                model, nn.Module
-            ), f'given custom network {type(model)}, torch.nn.Module expected'
-        if model is not None and isinstance(model, nn.Module):
             self.model = model
         else:
             self.config = AutoConfig.from_pretrained(model_name_or_path,
@@ -175,15 +236,34 @@ class LightningTransformer(pl.LightningModule):
             - list of optimizers.
             - list of lr schedulers.
         """
+        no_decay = ['bias', 'LayerNorm.weight']
         parameters = [
             {
-                'params': self.model.classifier.parameters()
+                'params': [
+                    p for n, p in self.model.named_parameters()
+                    if not any(nd in n for nd in no_decay)
+                ],
+                'weight_decay':
+                self.hparams.weight_decay,
             },
             {
-                'params': self.model.bert.parameters(),
-                'lr': self.hparams.encoder_learning_rate,
+                'params': [
+                    p for n, p in self.model.named_parameters()
+                    if any(nd in n for nd in no_decay)
+                ],
+                'weight_decay':
+                0.0,
             },
         ]
+        # parameters = [
+        #     {
+        #         'params': self.model.classifier.parameters()
+        #     },
+        #     {
+        #         'params': self.model.bert.parameters(),
+        #         'lr': self.hparams.encoder_learning_rate,
+        #     },
+        # ]
         optimizer = torch.optim.Adam(parameters,
                                      lr=self.hparams.learning_rate,
                                      eps=self.hparams.adam_epsilon)
