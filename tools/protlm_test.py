d9cd2001733fe3d20cb9d3d5500d84d9fd9a3aa7
jianzhnie <jianzhnie@126.com>
2022-05-09 08:10:33 +0200

    add protelm test

--- tools/protlm_test.py
+++ tools/protlm_test.py
@@ -1,21 +1,19 @@
 import sys
-
 from sklearn.metrics import average_precision_score, roc_auc_score
 from transformers import Trainer
-
+import pickle
+sys.path.append('../')
+from deepfold.loss.custom_metrics import compute_mcc, compute_roc
 from deepfold.data.protein_dataset import ProtBertDataset
 from deepfold.models.transformers.multilabel_transformer import \
     BertForMultiLabelSequenceClassification
 
-sys.path.append('../')
 
 
-def compute_metrics(pred):
-    labels = pred.label_ids
-    preds = pred.predictions
-    auc = roc_auc_score(labels, preds)
-    ap = average_precision_score(labels, preds)
-    return {'auc': auc, 'ap': ap}
+def compute_metrics(labels, preds):
+    mcc = compute_mcc(labels, preds)
+    roc = compute_roc(labels, preds)
+    return {'mcc': mcc, 'roc': roc}
 
 
 if __name__ == '__main__':
@@ -36,9 +34,17 @@ if __name__ == '__main__':
                                    max_length=512)
     num_classes = train_dataset.num_classes
     # Load trained model
-    model_path = 'work_dir/checkpoint-'
+    model_path = 'work_dir/checkpoint-6900'
     model = BertForMultiLabelSequenceClassification.from_pretrained(
         model_path, num_labels=num_classes)
     # Define test trainer
     test_trainer = Trainer(model)
-    predictions, label_ids, metrics = test_trainer.predict(val_dataset)
+    predictions = test_trainer.predict(test_dataset)
+    labels = predictions.label_ids
+    preds = predictions.predictions
+    preds = preds > 0.5
+    results = compute_metrics(labels, preds)
+    print(results)
+    results_path = 'work_dir/predictions.pkl'
+    with open(results_path, "wb") as f:
+        pickle.dump(predictions, f)
