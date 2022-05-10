3f87514f0f68d65920059beaa246fa884bafca06
Robin <jianzhnie@126.com>
2022-05-10 10:34:20 +0800

    Update esm_dataset.py

--- deepfold/data/esm_dataset.py
+++ deepfold/data/esm_dataset.py
@@ -1,7 +1,6 @@
-from typing import Dict, List, Tuple
-
+from typing import Dict
+import os
 import esm
-import numpy as np
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
@@ -10,8 +9,30 @@ from ..utils.constant import DEFAULT_ESM_MODEL, ESM_LIST
 
 
 class ESMDataset(Dataset):
-    def __init__(self, model_dir: str):
+
+    def __init__(self,
+                 data_path: str = 'dataset/',
+                 split: str = 'train',
+                 model_dir: str = None):
         super().__init__()
+
+        self.datasetFolderPath = data_path
+        self.trainFilePath = os.path.join(self.datasetFolderPath,
+                                          'train_data.pkl')
+        self.testFilePath = os.path.join(self.datasetFolderPath,
+                                         'test_data.pkl')
+        self.termsFilePath = os.path.join(self.datasetFolderPath, 'terms.pkl')
+
+        if split == 'train':
+            self.seqs, self.labels, self.terms = self.load_dataset(
+                self.trainFilePath, self.termsFilePath)
+        else:
+            self.seqs, self.labels, self.terms = self.load_dataset(
+                self.testFilePath, self.termsFilePath)
+
+        self.terms_dict = {v: i for i, v in enumerate(self.terms)}
+        self.num_classes = len(self.terms)
+
         if model_dir not in ESM_LIST:
             print(
                 f"Model dir '{model_dir}' not recognized. Using '{DEFAULT_ESM_MODEL}' as default"
@@ -21,36 +42,39 @@ class ESMDataset(Dataset):
         self._model, self.alphabet = esm.pretrained.load_model_and_alphabet(
             model_dir)
         self.batch_converter = self.alphabet.get_batch_converter()
-        self.data, self.labels = self.data_to_tensor(data_df, terms_dict)
 
     def __len__(self):
-        return self.len_df
+        return self.labels
 
     def __getitem__(self, idx):
-        data = self.data[idx, :]
-        label = self.labels[idx, :]
-        return data, label
 
-    def data_to_tensor(self, data_df, terms_dict):
-        labels = np.zeros((len(data_df), len(terms_dict)), dtype=np.int32)
-        esm_input = data_df.iloc[:, [1, 3]].values
+        seq = self.serqs[idx]
+        label_list = self.labels[idx]
+        multilabel = [0] * self.num_classes
+        for t_id in label_list:
+            if t_id in self.terms_dict:
+                label_idx = self.terms_dict[t_id]
+                multilabel[label_idx] = 1
 
-        convert_labels, convert_strs, convert_tokens = self.batch_converter(
-            esm_input)
+        return seq, multilabel
 
-        for i, row in enumerate(data_df.itertuples()):
-            for t_id in row.prop_annotations:
-                if t_id in terms_dict:
-                    labels[i, terms_dict[t_id]] = 1
+    def load_dataset(self, data_path, term_path):
+        df = pd.read_pickle(data_path)
+        terms_df = pd.read_pickle(term_path)
+        terms = terms_df['terms'].values.flatten()
 
-        labels = torch.from_numpy(labels).int()
+        seq = list(df['sequences'])
+        label = list(df['prop_annotations'])
+        assert len(seq) == len(label)
+        return seq, label, terms
 
-        return convert_tokens, labels
-
-    def process_sequences_and_tokens(
-            self, sequences_list: List[str]) -> Dict[str, torch.Tensor]:
+    def collate_fn(self, examples) -> Dict[str, torch.Tensor]:
         """Function to transform tokens string to IDs; it depends on the model
         used."""
+
+        sequences_list = [ex[0] for ex in examples]
+        multilabel_list = [ex[1] for ex in examples]
+
         if self.is_msa:
             _, _, all_tokens = self.batch_converter(sequences_list)
         else:
@@ -65,17 +89,5 @@ class ESMDataset(Dataset):
             1 * (all_tokens != self.token_to_id(self.pad_token)),
             'token_type_ids': torch.zeros(all_tokens.shape),
         }
+        encoded_inputs['labels'] = torch.tensor(multilabel_list)
         return encoded_inputs
-
-    def load_data(self, data_file, terms_file):
-        data_df = pd.read_pickle(data_file)
-        terms_df = pd.read_pickle(terms_file)
-        terms = terms_df['terms'].values.flatten()
-        return data_df, terms
-
-    def load_esm_model(self):
-        model, alphabet = torch.hub.load('facebookresearch/esm:main',
-                                         self.esm_model)
-        # 将未处理的 (labels + strings) batch 转化成(labels + tensor) batch
-        batch_converter = alphabet.get_batch_converter()
-        return batch_converter
