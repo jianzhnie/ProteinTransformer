68ebb1e105975ea884e9115fadc726d8bd553efb
Robin <jianzhnie@126.com>
2022-05-11 14:37:58 +0800

    Update esm_model.py

--- deepfold/models/esm_model.py
+++ deepfold/models/esm_model.py
@@ -9,12 +9,15 @@ from deepfold.utils.constant import DEFAULT_ESM_MODEL, ESM_LIST
 
 
 class ESMTransformer(nn.Module):
-    def __init__(self,
-                 model_dir: str,
-                 num_labels: int = 1000,
-                 max_len: int =1024,
-                 dropout_ratio: float = 0.0, 
-                 pooling_type='mean'):
+    def __init__(
+        self,
+        model_dir: str,
+        num_labels: int = 1000,
+        max_len: int = 1024,
+        dropout_ratio: float = 0.0,
+        pool_mode: Tuple[str, ...] = ('cls', 'first-last-avg', 'last-avg',
+                                      'pooler'),
+    ):
         super().__init__()
 
         if model_dir not in ESM_LIST:
@@ -26,6 +29,7 @@ class ESMTransformer(nn.Module):
         self._model, self.alphabet = esm.pretrained.load_model_and_alphabet(
             model_dir)
         self.num_layers = self._model.num_layers
+        # esm1b: 33 layers
         repr_layers = -1
         self.repr_layers = (repr_layers + self.num_layers +
                             1) % (self.num_layers + 1)
@@ -34,8 +38,10 @@ class ESMTransformer(nn.Module):
 
         self.num_labels = num_labels
         self.max_len = max_len
+        self.pool_mode = pool_mode
         self.dropout = nn.Dropout(dropout_ratio)
         self.classifier = nn.Linear(self.hidden_size, num_labels)
+        self._freeze_backbone
 
     def _freeze_backbone(self):
         for p in self._model.parameters():
@@ -46,6 +52,7 @@ class ESMTransformer(nn.Module):
                 attention_mask=None,
                 token_type_ids=None,
                 position_ids=None,
+                lengths=None,
                 labels=None) -> Tuple[torch.Tensor, torch.Tensor]:
         """Function which computes logits and embeddings based on a list of
         sequences, a provided batch size and an inference configuration. The
@@ -66,10 +73,39 @@ class ESMTransformer(nn.Module):
             input_ids,
             repr_layers=[self.repr_layers],
         )
+
         # batch_logits = model_outputs['logits']
         batch_embeddings = model_outputs['representations'][self.repr_layers]
-        batch_embeddings = torch.mean(batch_embeddings[:, 1:self.max_len +1], dim=1)
-        pooled_output = self.dropout(batch_embeddings)
+        # batch_embeddings: batch_size * seq_length * embedding_dim
+        batch_embeddings = [emb for emb in batch_embeddings]
+
+        # Remove class token and padding
+        # Use tranpose to filter on the two last dimensions. Doing this, we don't have to manage
+        # the first dimension of the tensor. It works for [dim1, dim2, token_size, emb_size] and
+        # for [dim1, token_size, emb_size]
+
+        filtered_embeddings = [
+            emb.transpose()[:, 1:(length + 1)].transpose()
+            for emb, length in zip(batch_embeddings, lengths)
+        ]
+        # if self.pool_mode in 'first-last-avg':
+        #     outputs = [
+        #         nn.AdaptiveAvgPool1d(batch_embeddings[0]),
+        #         nn.AdaptiveAvgPool1d(batch_embeddings[-1])
+        #     ]
+        #     embeddings = torch.mean(outputs)
+        if self.pool_mode in 'mean':
+            embeddings = torch.stack([
+                emb.transpose().mean(1).transpose()
+                for emb in filtered_embeddings
+            ])
+        # keep class token only
+        elif self.pool_mode in 'cls':
+            embeddings = torch.stack([
+                emb.transpose()[:, 0].transpose() for emb in batch_embeddings
+            ])
+
+        pooled_output = self.dropout(embeddings)
         logits = self.classifier(pooled_output)
 
         outputs = (logits, )
