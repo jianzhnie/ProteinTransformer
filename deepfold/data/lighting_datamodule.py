4f96d24e3851fe787233326ab8ac6a80310ab110
Robin <jianzhnie@126.com>
2022-05-13 13:55:28 +0800

    update main

--- deepfold/data/lighting_datamodule.py
+++ deepfold/data/lighting_datamodule.py
@@ -1,6 +1,7 @@
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 
+from .esm_dataset import ESMDataset
 from .protein_dataset import ProtBertDataset
 
 
@@ -45,3 +46,50 @@ class LightingSeqenceDataModule(pl.LightningDataModule):
 
     def test_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=self.batch_size)
+
+
+class ESMDataModule(pl.LightningDataModule):
+    """pytorch_lighting seqence Dataset Model."""
+    def __init__(self,
+                 data_path: str = 'dataset/',
+                 model_name: str = None,
+                 batch_size: int = 16,
+                 max_length: int = 1024):
+        super().__init__()
+
+        self.data_path = data_path
+        self.model_name = model_name
+        self.batch_size = batch_size
+        self.max_token_len = max_length
+
+    def setup(self, stage: str):
+        self.train_dataset = ESMDataset(data_path=self.data_path,
+                                        split='train',
+                                        model_dir=self.model_name,
+                                        max_length=self.max_token_len)
+
+        self.valid_dataset = ESMDataset(data_path=self.data_path,
+                                        split='valid',
+                                        model_dir=self.model_name,
+                                        max_length=self.max_token_len)
+        self.test_dataset = ESMDataset(data_path=self.data_path,
+                                       split='test',
+                                       model_dir=self.model_name,
+                                       max_length=self.max_token_len)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=True,
+                          num_workers=4,
+                          pin_memory=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.valid_dataset,
+                          batch_size=self.batch_size,
+                          pin_memory=True)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset,
+                          batch_size=self.batch_size,
+                          pin_memory=True)
