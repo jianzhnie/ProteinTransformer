ccdf1d3a13c8a3500c3b234b681ff8df88ffc169
Robin <jianzhnie@126.com>
2022-05-14 22:34:56 +0800

    update esm model

--- scripts/ditribute_training.sh
+++ scripts/ditribute_training.sh
@@ -5,9 +5,24 @@ python function_predcit.py \
 
 
 python main.py \
---data_path /home/niejianzheng/xbiome/dataset/protein \
+--data_path /home/niejianzheng/xbiome/datasets/protein \
 --output-dir /home/niejianzheng/xbiome/DeepFold/work_dir \
 --epochs 2 \
 --lr 0.001 \
 --epochs 10 \
---batch-size 32
+--batch-size 1
+
+## local machine
+python main.py \
+--data_path /Users/robin/xbiome/datasets/protein \
+--output-dir /Users/robin/xbiome/DeepFold/work_dir \
+--epochs 2 \
+--lr 0.001 \
+--epochs 10 \
+--batch-size 1
+
+#
+python -m torch.distributed.launch --nproc_per_node=2 main_amp.py -a resnet50 --b 224 --workers 4 --opt-level O1 ./
+python main_amp.py -a resnet50 --b 224 --workers 4 --opt-level O2 ./
+python main_amp.py -a resnet50 --b 224 --workers 4 --opt-level O2 --loss-scale 128.0 ./
+python -m torch.distributed.launch --nproc_per_node=2 main_amp.py -batch-size 8 --workers 4 --opt-level O2  --loss-scale 128.0
--- scripts/train_model.sh
+++ scripts/train_model.sh
@@ -10,3 +10,7 @@ python function_predcit.py \
         --data-path /home/niejianzheng/xbiome/DeepFold/data/data \
         --model-path /home/niejianzheng/xbiome/DeepFold/work_dir/model/deepgoplus \
         --summary-path home/niejianzheng/xbiome/DeepFold/work_dir/logs/deepgoplus
+
+
+
+nohup python protlm.py > results.log 2>&1 &
