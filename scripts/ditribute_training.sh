4d976baefa6b9006a69f4ec0efa63a1475872e9a
Robin <jianzhnie@126.com>
2022-05-17 14:30:47 +0800

    Update ditribute_training.sh

--- scripts/ditribute_training.sh
+++ scripts/ditribute_training.sh
@@ -7,20 +7,13 @@
 # --epochs 10 \
 # --batch-size 1
 
-# #####
-# python -m torch.distributed.launch --nproc_per_node=2 main_amp.py -a resnet50 --b 224 --workers 4 --opt-level O1 ./
-# python main_amp.py -a resnet50 --b 224 --workers 4 --opt-level O2 ./
-# python main_amp.py -a resnet50 --b 224 --workers 4 --opt-level O2 --loss-scale 128.0 ./
-# python -m torch.distributed.launch --nproc_per_node=2 main_amp.py -batch-size 8 --workers 4 --opt-level O2  --loss-scale 128.0
-# ####
-
 # # single gpu
-# python main.py \
-# --data_path /home/niejianzheng/xbiome/datasets/protein \
-# --output-dir /home/niejianzheng/xbiome/DeepFold/work_dir \
-# --lr 0.001 \
-# --epochs 10 \
-# --batch-size 8
+python main.py \
+--data_path /home/niejianzheng/xbiome/datasets/protein \
+--output-dir /home/niejianzheng/xbiome/DeepFold/work_dir \
+--lr 0.001 \
+--epochs 10 \
+--batch-size 2
 
 ## distributed
 nohup  python -m torch.distributed.launch --nnodes=1 --nproc_per_node=2  main.py  \
