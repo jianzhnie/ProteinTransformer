c2e457ff8ca8bf6b64e0cbaef10bb5df3e174395
Robin <jianzhnie@126.com>
2022-05-17 16:25:28 +0800

    Update modeling_lstm.py

--- deepfold/models/modeling_lstm.py
+++ deepfold/models/modeling_lstm.py
@@ -4,7 +4,6 @@ import torch
 import torch.functional as F
 import torch.nn as nn
 from tape.models.modeling_utils import ProteinModel
-from torch.nn import BCEWithLogitsLoss
 
 URL_PREFIX = 'https://s3.amazonaws.com/songlabdata/proteindata/pytorch-models/'
 LSTM_PRETRAINED_CONFIG_ARCHIVE_MAP: typing.Dict[str, str] = {}
@@ -164,6 +163,33 @@ class ProteinLSTMModel(ProteinLSTMAbstractModel):
         return outputs  # sequence_output, pooled_output, (hidden_states)
 
 
+class MultilabelProteinLSTMModel(ProteinLSTMModel):
+    def __init__(self, config):
+        super().__init__(config)
+
+        self.config = config
+        self.num_labels = config.num_labels
+        self.protlstm = ProteinLSTMModel(config)
+        classifier_dropout = (config.classifier_dropout
+                              if config.classifier_dropout is not None else
+                              config.hidden_dropout_prob)
+        self.dropout = nn.Dropout(classifier_dropout)
+        self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
+        self.init_weights()
+
+    def forward(
+        self,
+        input_ids,
+        labels=None,
+    ):
+        outputs = self.protlstm(input_ids)
+        pooled_output = outputs[1]
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+        outputs = (logits, ) + outputs[2:]
+        return outputs  # (loss), logits, (hidden_states), (attentions)
+
+
 class LstmEncoderModel(nn.Module):
     """LstmEncoderModel."""
     def __init__(self,
@@ -192,83 +218,3 @@ class LstmEncoderModel(nn.Module):
         encoder_output, _ = self.lstm_encoder(token_embed)
         encoder_output = self.dropout(encoder_output)
         return encoder_output
-
-
-class PretrainTaskModel(nn.Layer):
-    """PretrainTaskModel."""
-    def __init__(self, class_num, model_config, encoder_model):
-        """__init__"""
-        super(PretrainTaskModel, self).__init__()
-
-        model_type = model_config.get('model_type', 'transformer')
-        hidden_size = model_config.get('hidden_size', 512)
-        in_channels = hidden_size * 2 if model_type == 'lstm' else hidden_size
-
-        self.conv_decoder = nn.Sequential(
-            nn.Conv1D(in_channels=in_channels,
-                      out_channels=128,
-                      kernel_size=5,
-                      padding='same',
-                      data_format='NLC'),
-            nn.ReLU(),
-            nn.Conv1D(in_channels=128,
-                      out_channels=class_num,
-                      kernel_size=3,
-                      padding='same',
-                      data_format='NLC'),
-        )
-        self.encoder_model = encoder_model
-
-    def forward(self, input, pos):
-        """forward."""
-        encoder_output = self.encoder_model(input, pos)
-        decoder_output = self.conv_decoder(encoder_output)
-
-        return decoder_output
-
-
-class MultiLabelSequenceClassification():
-    """Bert model adapted for multi-label sequence classification."""
-    def __init__(self, config, pos_weight=None):
-        super(self).__init__(config)
-        self.num_labels = config.num_labels
-        self.lstm = LstmEncoderModel(config)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
-        self.pos_weight = pos_weight
-
-        self.init_weights()
-
-    def forward(
-        self,
-        input_ids,
-        attention_mask=None,
-        token_type_ids=None,
-        position_ids=None,
-        head_mask=None,
-        labels=None,
-    ):
-        outputs = self.bert(
-            input_ids,
-            attention_mask=attention_mask,
-            token_type_ids=token_type_ids,
-            position_ids=position_ids,
-            head_mask=head_mask,
-        )
-
-        pooled_output = outputs[1]
-
-        pooled_output = self.dropout(pooled_output)
-        logits = self.classifier(pooled_output)
-
-        outputs = (logits, ) + outputs[
-            2:]  # add hidden states and attention if they are here
-
-        if labels is not None:
-            loss_fct = BCEWithLogitsLoss(pos_weight=self.pos_weight)
-            labels = labels.float()
-            loss = loss_fct(logits.view(-1, self.num_labels),
-                            labels.view(-1, self.num_labels))
-            outputs = (loss, ) + outputs
-
-        return outputs  # (loss), logits, (hidden_states), (attentions)
