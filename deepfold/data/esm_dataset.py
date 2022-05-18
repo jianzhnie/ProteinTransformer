13bd3df2c86414e6bfb1ff0e00d0ab2fda8f2458
Robin <jianzhnie@126.com>
2022-05-18 18:11:29 +0800

    Update esm_dataset.py

--- deepfold/data/esm_dataset.py
+++ deepfold/data/esm_dataset.py
@@ -142,6 +142,8 @@ class ESMDataset(Dataset):
                 ('', sequence) for sequence in sequences_list
             ])
 
+        # The model is trained on truncated sequences and passing longer ones in at
+        # infernce will cause an error. See https://github.com/facebookresearch/esm/issues/21
         if self.truncate:
             all_tokens = all_tokens[:, :self.max_length - 2]
 
