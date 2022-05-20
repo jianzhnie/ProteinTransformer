584ba3972b578e04889520e2b33aaa988b8e77fb
Robin <jianzhnie@126.com>
2022-05-20 11:43:54 +0800

    Update extract_embedding.sh

--- scripts/extract_embedding.sh
+++ scripts/extract_embedding.sh
@@ -1,11 +1,10 @@
 python extract_embeddings.py  \
 --data_path /home/af2/xbiome/data/protein_classification \
---output-dir /home/af2/xbiome/DeepFold/work_dir \
 --split "test" \
---batch-size 16 
+--batch-size 16
 
-## 
+## 108
 python extract_embeddings.py  \
 --data_path /home/niejianzheng/xbiome/datasets/protein \
 --split "train" \
---batch-size 32
+--batch-size 64
