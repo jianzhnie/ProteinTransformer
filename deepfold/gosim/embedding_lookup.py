3e81487403f440a05f0f9d65104bd30e71e24424
Robin <jianzhnie@126.com>
2022-05-30 21:22:49 +0800

    update go sim predict

---
+++ deepfold/gosim/embedding_lookup.py
@@ -0,0 +1,41 @@
+import sys
+
+import numpy
+from sklearn.metrics import pairwise, pairwise_distances
+
+
+class EmbeddingLookup(object):
+    def __init__(self, embedding_db):
+        self.embedding_db = embedding_db
+        # prepare data
+        self.ids, self.embedding_mat = zip(*self.embedding_db.items())
+
+    def run_embedding_lookup_distance(self, querys, metric):
+        """Calculate embedding distance of all querys against the lookup
+        database.
+
+        :param querys: querys for which distances should be calculated
+        :param metric: metric to use to calculate distances
+        :return: distances, query ids
+        """
+
+        if metric in pairwise.distance_metrics():
+            if isinstance(querys, dict):
+                query_ids, raw_data_query = zip(*querys.items())
+            else:
+                raw_data_query = querys
+                query_ids = range(0, numpy.shape(querys)[0])
+
+            raw_data_query = numpy.array(raw_data_query).squeeze()
+            if len(query_ids) == 1:
+                raw_data_query = raw_data_query.reshape(1, -1)
+
+            distances = pairwise_distances(raw_data_query,
+                                           self.embedding_mat,
+                                           metric=metric)
+        else:
+            sys.exit('{} is not a correct distance metric\n'
+                     'See <sklearn.metrics.pairwise.distance_metrics()> '
+                     'for all possible distance metrics'.format(metric))
+
+        return distances, query_ids
