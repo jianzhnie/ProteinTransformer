132ae170d0ba09f6035e50ca443e59b202510587
Robin <jianzhnie@126.com>
2022-06-08 18:31:44 +0800

    update

--- deepfold/models/model_factory.py
+++ deepfold/models/model_factory.py
@@ -1,24 +1,22 @@
-from deepgoplus import DeepGOPlusModel
-from esm_model import EsmEmbeddingModel, EsmTransformer
-from modeling_lstm import MultilabelProteinLSTMModel, ProteinLSTMConfig
+from .deepgoplus import DeepGOPlusModel
+from .esm_model import EsmEmbeddingModel, EsmTransformer
+from .modeling_lstm import MultilabelProteinLSTMModel, ProteinLSTMConfig
 
 
 def get_model(args):
-    model_name = args.model_name
-
-    if model_name == 'deepgoplus':
+    if args.model == 'deepgoplus':
         model = DeepGOPlusModel(args.num_labels)
 
-    if model_name == 'esm':
+    if args.model == 'esm':
 
         model = EsmTransformer(num_labels=args.num_labels,
                                pool_mode=args.pool_mode,
                                fintune=False)
-    if model_name == 'esm_embedding':
+    if args.model == 'esm_embedding':
 
         model = EsmEmbeddingModel(input_size=1280, num_labels=args.num_labels)
 
-    if model_name == 'lstm':
+    if args.model == 'lstm':
 
         config = ProteinLSTMConfig(num_labels=args.num_labels)
         model = MultilabelProteinLSTMModel(config)
