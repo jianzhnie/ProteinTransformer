50120573fb8a6ac74df92719c2fc064f179b8228
Robin <jianzhnie@126.com>
2022-06-20 10:05:56 +0800

    Update inference_protlm.py

--- tools/inference_protlm.py
+++ tools/inference_protlm.py
@@ -12,12 +12,14 @@ import torch.utils.data.distributed
 import yaml
 from torch.utils.data import DataLoader
 from transformers import RobertaConfig
-sys.path.append('../')
+
 from deepfold.data.protein_dataset import ProtRobertaDataset
 from deepfold.models.transformers.multilabel_transformer import \
     RobertaForMultiLabelSequenceClassification
 from deepfold.trainer.training import ProtLMPredict
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path.split('/')[:-1])
 
 # The first arg parser parses out only the --config argument, this argument is used to
 # load a yaml file containing key-values that override the defaults for the main parser below
