775625c0ab3cfe74328b8e5992d07621aeba1ff6
jianzhnie <jianzhnie@126.com>
2022-06-23 12:36:49 +0200

    add extract embedding model

--- tools/extract_ontotextual_embeddings.py
+++ tools/extract_ontotextual_embeddings.py
@@ -2,17 +2,17 @@ import argparse
 import logging
 import os
 import sys
-
+import numpy as np
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import torch
 import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader
-from transformers import AutoModelForSequenceClassification
-
+from transformers import AutoModel
+sys.path.append('../')
 from deepfold.data.ontotextual_dataset import OntoTextDataset
 from deepfold.trainer.embeds import extract_sentence_embedds
 
-sys.path.append('../')
 
 parser = argparse.ArgumentParser(
     description='Protein function Classification Model Train config')
@@ -52,16 +52,15 @@ def main(args):
     data_file = os.path.join(args.data_path, 'GotermText.csv')
     assert os.path.exists(data_file)
     save_path = os.path.join(
-        args.data_path,
-        + 'onto_embeddings_' + args.pool_mode + '_' + args.split + '.pkl')
+        args.data_path, 'onto_embeddings_' + args.pool_mode + '_' + args.split + '.pkl')
     print('Pretrained model %s, pool_mode: %s,  file path: %s' %
           (model_name, args.pool_mode, data_file))
     print('Embeddings save path: ', save_path)
-
     # Dataset and DataLoader
     dataset = OntoTextDataset(data_dir=args.data_path,
                               tokenizer_name=model_name,
-                              max_length=1024)
+                              max_length=512)
+
     # dataloders
     data_loader = DataLoader(dataset,
                              batch_size=args.batch_size,
@@ -69,7 +68,7 @@ def main(args):
                              num_workers=args.workers,
                              pin_memory=True)
     # model
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    model = AutoModel.from_pretrained(model_name)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
     # run predict
@@ -79,9 +78,12 @@ def main(args):
                                           logger=logger,
                                           device=device)
     print(embeddings.shape)
+
+
     df = pd.read_csv(data_file)
     df['embeddings'] = embeddings.tolist()
     df.to_csv(save_path)
+
     print('Embeddings saved to :', save_path)
 
 
