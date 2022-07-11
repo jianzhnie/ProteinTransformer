d0728ca7317185cdd5f40c0d2c23f8eef832e80c
jianzhnie <jianzhnie@126.com>
2022-07-11 09:26:32 +0200

    update scripts

--- scripts/108/extract_embedding.sh
+++ scripts/108/extract_embedding.sh
@@ -31,3 +31,18 @@ python extract_ontotextual_embeddings.py \
 --pretrain_model_dir /home/niejianzheng/xbiome/DeepFold/work_dir/ontotextual_embeddings/checkpoint/checkpoint-1650 \
 --embedding_file_name bert_fintune.pkl \
 --batch-size 256
+
+
+python extract_ontotextual_embeddings.py \
+--data_path /home/niejianzheng/xbiome/datasets/protein/cafa3/process \
+--obo_file  /home/niejianzheng/xbiome/datasets/protein/cafa3/process/go_cafa3.obo \
+--tokenizer_dir microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext \
+--pretrain_model_dir microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext \
+--embedding_file_name bert_embedding.pkl \
+--batch-size 256
+
+
+python extract_esm_embeddings.py \
+--data_path /home/niejianzheng/xbiome/datasets/protein/cafa3/process \
+--split train \
+--batch-size 64 \
--- scripts/108/run_evaluation.sh
+++ scripts/108/run_evaluation.sh
@@ -5,17 +5,6 @@ python  evaluate_diamondscore.py \
     --diamond-scores-file /Users/robin/xbiome/datasets/protein/test_diamond.res \
     --ontology-obo-file /Users/robin/xbiome/datasets/protein/go.obo
 
-
-
-## af2
-python  tools/evaluate_deepmodel.py \
-    --train-data-file /data/xbiome/protein_classification/train_data.pkl \
-    --test-data-file /data/xbiome/protein_classification/bert_embedding_predictions.pkl \
-    --terms-file /data/xbiome/protein_classification/terms.pkl \
-    --ontology-obo-file /data/xbiome/protein_classification/go.obo \
-    --output_dir /home/af2/xbiome/DeepFold/work_dir
-
-
 ## 108
 ## evaluate diamond
 python  evaluate_diamondscore.py \
@@ -37,7 +26,15 @@ python  evaluate_deepmodel.py \
 ## evaluate go_sim
 python  evaluate_gosim.py \
     --train-data-file /home/niejianzheng/xbiome/datasets/protein/train_data.pkl \
-    --test-data-file /home/niejianzheng/xbiome/datasets/protein/predictions.pkl \
-    --gosim-scores-file /home/niejianzheng/xbiome/goPredSim/results/all_predicts.txt \
-    --ontology-obo-file /home/niejianzheng/xbiome/datasets/protein/go.obo \
+    --test-data-file /home/niejianzheng/xbiome/datasets/protein/cco_predictions.pkl \
+    --terms-file /home/niejianzheng/xbiome/datasets/protein/cco_terms.pkl \
+    --ontology-obo-file /home/niejianzheng/xbiome/datasets/protein/go_cafa3.obo \
+    --output_dir /home/niejianzheng/xbiome/DeepFold/work_dir
+
+## evaluate model
+python  evaluate_deepmodel.py \
+    --train-data-file /home/niejianzheng/xbiome/DeepFold/protein_data/cafa3/process/cco/cco_train_data.pkl \
+    --test-data-file /home/niejianzheng/xbiome/DeepFold/protein_data/cafa3/process/cco_predictions.pkl \
+    --terms-file /home/niejianzheng/xbiome/DeepFold/protein_data/cafa3/process/cco_terms.pkl \
+    --ontology-obo-file /home/niejianzheng/xbiome/DeepFold/protein_data/cafa3/process/go_cafa3.obo \
     --output_dir /home/niejianzheng/xbiome/DeepFold/work_dir
